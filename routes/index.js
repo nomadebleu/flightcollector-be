4293edc1992d9e04b373750ec6c8175da399fa6b
Anthony <anthonyiezzi64@gmail.com>
2024-02-10 23:35:14 +0100

    route signup avec regex password

--- routes/index.js
+++ routes/index.js
@@ -17,8 +17,14 @@ router.post('/signup', async (req, res) => {
     const { firstname, lastname, password, mail } = req.body;
     const hash = bcrypt.hashSync(password, 10); // Utilise bcrypt.hashSync pour hacher le mdp
 
-    if (!checkBody(req.body, ['firstname','lastname', 'mail','password'])) {
+    if (!checkBody(req.body, ['firstname', 'lastname', 'mail', 'password'])) {
       res.json({ result: false, error: 'Missing or empty fields' });
+      return;
+    }
+     // Validation du mot de passe avec Maj/Number/Special Caracter
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+    if (!passwordRegex.test(password)) {
+      res.json({ result: false, error: 'Password must contain at least 8 characters, including uppercase, lowercase, digit, and special character' });
       return;
     }
 
