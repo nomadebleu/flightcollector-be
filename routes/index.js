2a4fa62196ece419357be8094ae53263f00cd698
Vanessa <vane.giraldo14@gmail.com>
2024-02-12 22:06:37 +0100

    cloudinary cloud config

--- routes/index.js
+++ routes/index.js
@@ -1,27 +1,37 @@
-require('../models/connection');
-var express = require('express');
+require("../models/connection");
+var express = require("express");
 var router = express.Router();
 
-const User = require('../models/users');
-const { checkBody } = require('../modules/checkBody');
+const User = require("../models/users");
+const { checkBody } = require("../modules/checkBody");
 
-const uid2 = require('uid2');
+const uid2 = require("uid2");
 const token = uid2(32);
 
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcrypt");
+
+const cloudinary = require("cloudinary").v2;
+const uniqid = require("uniqid");
+const fs = require("fs");
+
+cloudinary.config({
+  cloud_name: "ddreb3nm6",
+  api_key: "342387261832956",
+  api_secret: "lzxO7d6dYHFjVbjLR8veX1A9b-8",
+});
 
 // SIGNUP
-router.post('/signup', async (req, res) => {
-  console.log('ok');
+router.post("/signup", async (req, res) => {
+  console.log("ok");
   try {
     const { firstname, lastname, password, mail } = req.body;
     const hash = bcrypt.hashSync(password, 10); // Utilise bcrypt.hashSync pour hacher le mdp
 
     const { result, errors } = checkBody(req.body, [
-      'firstname',
-      'lastname',
-      'mail',
-      'password',
+      "firstname",
+      "lastname",
+      "mail",
+      "password",
     ]);
 
     if (!result) {
@@ -33,7 +43,7 @@ router.post('/signup', async (req, res) => {
     const userData = await User.findOne({ mail });
 
     if (userData) {
-      res.json({ result: false, error: 'User already exists' });
+      res.json({ result: false, error: "User already exists" });
     } else {
       const newUser = new User({
         firstname,
@@ -48,23 +58,20 @@ router.post('/signup', async (req, res) => {
     }
   } catch (error) {
     console.error("Une erreur s'est produite :", error);
-    res.status(500).json({ result: false, error: 'Erreur interne du serveur' });
+    res.status(500).json({ result: false, error: "Erreur interne du serveur" });
   }
 });
 
 //SIGNIN
-router.post('/signin', async (req, res) => {
+router.post("/signin", async (req, res) => {
   try {
     const { mail, password } = req.body;
 
-    if (!checkBody(req.body, ['mail', 'password'])) {
-      res.json({ result: false, error: 'Missing or empty fields' });
+    if (!checkBody(req.body, ["mail", "password"])) {
+      res.json({ result: false, error: "Missing or empty fields" });
       return;
     }
-    const { result, errors } = checkBody(req.body, [
-      'mail',
-      'password',
-    ]);
+    const { result, errors } = checkBody(req.body, ["mail", "password"]);
 
     if (!result) {
       res.json({ result: false, errors }); // Renvoie les erreurs
@@ -76,11 +83,11 @@ router.post('/signin', async (req, res) => {
     if (userData && bcrypt.compareSync(password, userData.password)) {
       res.json({ result: true, userData });
     } else {
-      res.json({ result: false, error: 'Mail or Password invalid' });
+      res.json({ result: false, error: "Mail or Password invalid" });
     }
   } catch (error) {
     console.error("Une erreur s'est produite :", error);
-    res.status(500).json({ result: false, error: 'Erreur interne du serveur' });
+    res.status(500).json({ result: false, error: "Erreur interne du serveur" });
   }
 });
 
--- routes/users.js
+++ routes/users.js
@@ -1,17 +1,17 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const User = require('../models/users'); 
-const bcrypt = require('bcrypt'); // Pour le hachage des mots de passe
-const Badge = require('../models/badges')
-const Flight = require('../models/flights')
+const User = require("../models/users");
+const bcrypt = require("bcrypt"); // Pour le hachage des mots de passe
+const Badge = require("../models/badges");
+const Flight = require("../models/flights");
 
 /* GET home page. */
-router.get('/', function(req, res) {
-  res.json({result:true});
+router.get("/", function (req, res) {
+  res.json({ result: true });
 });
 
-//Route GET pour récupérer toute les infos de l'utilisateur : 
-router.put('/:userId/flight', async (req, res) => {
+//Route GET pour récupérer toute les infos de l'utilisateur :
+router.put("/:userId/flight", async (req, res) => {
   const userId = req.params.userId;
   const { flight } = req.body; // Les nouveaux vols à assigner à l'utilisateur
 
@@ -24,24 +24,33 @@ router.put('/:userId/flight', async (req, res) => {
 
     if (updateResult.nModified === 0) {
       // Aucune modification effectuée, utilisateur non trouvé
-      return res.status(404).json({ error: 'Utilisateur non trouvé' });
+      return res.status(404).json({ error: "Utilisateur non trouvé" });
     }
 
     // Répondre avec un message indiquant que les vols de l'utilisateur ont été mis à jour avec succès
-    res.status(200).json({ message: 'Les vols de l\'utilisateur ont été mis à jour avec succès' });
+    res
+      .status(200)
+      .json({
+        message: "Les vols de l'utilisateur ont été mis à jour avec succès",
+      });
   } catch (error) {
     // En cas d'erreur, renvoyer une réponse d'erreur avec le code d'erreur approprié
-    console.error('Erreur lors de la mise à jour des vols de l\'utilisateur :', error);
-    res.status(500).json({ error: 'Erreur lors de la mise à jour des vols de l\'utilisateur' });
+    console.error(
+      "Erreur lors de la mise à jour des vols de l'utilisateur :",
+      error
+    );
+    res
+      .status(500)
+      .json({
+        error: "Erreur lors de la mise à jour des vols de l'utilisateur",
+      });
   }
 });
-
-
 
 /*Route PUT */
 
 //PUT /password :Changer le password
-router.put('/password', async (req, res) => {
+router.put("/password", async (req, res) => {
   try {
     // Récupérer les données du corps de la requête
     const { mail, newPassword } = req.body;
@@ -62,15 +71,17 @@ router.put('/password', async (req, res) => {
     await data.save();
 
     // Renvoyez une réponse de succès
-    res.json({ result:true, message: "Password updated successfully" });
+    res.json({ result: true, message: "Password updated successfully" });
   } catch (error) {
     console.error("Une erreur s'est produite :", error);
-    res.status(500).json({ error: "Erreur lors de la mise à jour du mot de passe" });
+    res
+      .status(500)
+      .json({ error: "Erreur lors de la mise à jour du mot de passe" });
   }
 });
 
-//Route Put pour mettre à jour le nombre de point de l'utilisateur : 
-router.put('/pointsTotal/:userId', async (req, res) => {
+//Route Put pour mettre à jour le nombre de point de l'utilisateur :
+router.put("/pointsTotal/:userId", async (req, res) => {
   try {
     // Récupére l'ID de l'utilisateur à partir de la requête
     const { userId } = req.params;
@@ -91,18 +102,24 @@ router.put('/pointsTotal/:userId', async (req, res) => {
     await user.save();
 
     // Renvoyer une réponse de succès
-    res.json({ message: "Nombre de points de l'utilisateur mis à jour avec succès" });
+    res.json({
+      message: "Nombre de points de l'utilisateur mis à jour avec succès",
+    });
   } catch (err) {
     console.error(err);
-    res.status(500).json({ message: "Erreur lors de la mise à jour du nombre total de points de l'utilisateur" });
+    res
+      .status(500)
+      .json({
+        message:
+          "Erreur lors de la mise à jour du nombre total de points de l'utilisateur",
+      });
   }
 });
-
 
 /*Route POST */
 
 //POST /ajouter un badge à la BADGES de user
-router.post('/addBadges', async (req, res) => {
+router.post("/addBadges", async (req, res) => {
   try {
     // Récupérer les données du corps de la requête
     const { userId, badgeId } = req.body;
@@ -117,7 +134,9 @@ router.post('/addBadges', async (req, res) => {
 
     // Vérifier si l'utilisateur possède déjà le badge
     if (user.badges.includes(badgeId)) {
-      return res.status(400).json({ message: "L'utilisateur possède déjà ce badge" });
+      return res
+        .status(400)
+        .json({ message: "L'utilisateur possède déjà ce badge" });
     }
 
     // Rechercher le badge dans la base de données
@@ -138,68 +157,95 @@ router.post('/addBadges', async (req, res) => {
     res.json({ message: "Badge ajouté avec succès à l'utilisateur" });
   } catch (error) {
     console.error("Une erreur s'est produite :", error);
-    res.status(500).json({ error: "Erreur lors de l'ajout du badge à l'utilisateur" });
+    res
+      .status(500)
+      .json({ error: "Erreur lors de l'ajout du badge à l'utilisateur" });
   }
 });
 
+//UPLOAD PHOTO PROFILE
+router.post("/upload", async (req, res) => {
+  const photoPath = `./tmp/${uniqid()}.jpg`;
+  const resultMove = await req.files.photoFromFront.mv(photoPath);
 
+  if (!resultMove) {
+    const resultCloudinary = await cloudinary.uploader.upload(photoPath);
+    res.json({ result: true, url: resultCloudinary.secure_url });
+    console.log({ result: true, url: resultCloudinary.secure_url });
+  } else {
+    res.json({ result: false, error: resultMove });
+  }
 
+  fs.unlinkSync(photoPath);
+});
 
-router.post('/associateFlights/:userId', async (req, res) => {
+router.post("/associateFlights/:userId", async (req, res) => {
   try {
     const userId = req.params.userId;
-    const { flightIds } = req.body; 
+    const { flightIds } = req.body;
 
     // Recherche de l'utilisateur dans la base de données
     const user = await User.findById(userId);
     if (!user) {
-      return res.status(404).json({ error: 'Utilisateur non trouvé' });
+      return res.status(404).json({ error: "Utilisateur non trouvé" });
     }
 
     // Fusionner les nouveaux vols avec les vols existants de l'utilisateur
-    user.flights = [...user.flights, ...flightIds]; 
+    user.flights = [...user.flights, ...flightIds];
 
     // Enregistre les modifications dans la base de données
     await user.save();
 
-    return res.status(200).json({ message: 'Vols associés à l\'utilisateur avec succès' });
+    return res
+      .status(200)
+      .json({ message: "Vols associés à l'utilisateur avec succès" });
   } catch (error) {
-    console.error('Erreur lors de l\'association des vols à l\'utilisateur :', error);
-    return res.status(500).json({ error: 'Erreur serveur lors de l\'association des vols à l\'utilisateur' });
+    console.error(
+      "Erreur lors de l'association des vols à l'utilisateur :",
+      error
+    );
+    return res
+      .status(500)
+      .json({
+        error: "Erreur serveur lors de l'association des vols à l'utilisateur",
+      });
   }
 });
-
-
-
-
 
 /*Route GET */
 
 // Route pour récupérer les infos de tous les vols de l'utilisateur
-router.get('/userFlightInfo/:userId', async (req, res) => {
+router.get("/userFlightInfo/:userId", async (req, res) => {
   try {
     const userId = req.params.userId;
 
     // Recherche de l'utilisateu r dans la base de données avec tous les vols associés
-    const user = await User.findById(userId).populate('flights'); 
+    const user = await User.findById(userId).populate("flights");
     if (!user) {
-      return res.status(404).json({ error: 'Utilisateur non trouvé' });
+      return res.status(404).json({ error: "Utilisateur non trouvé" });
     }
 
     // Si l'utilisateur n'a pas de vol associé
     if (!user.flights || user.flights.length === 0) {
-      return res.status(400).json({ error: 'L\'utilisateur n\'a pas de vol associé' });
+      return res
+        .status(400)
+        .json({ error: "L'utilisateur n'a pas de vol associé" });
     }
 
     // Renvoyer toutes les informations des vols associés
     return res.status(200).json({ flightsInfo: user.flights });
   } catch (error) {
-    console.error('Erreur lors de la récupération des infos des vols de l\'utilisateur :', error);
-    return res.status(500).json({ error: 'Erreur serveur lors de la récupération des infos des vols de l\'utilisateur' });
+    console.error(
+      "Erreur lors de la récupération des infos des vols de l'utilisateur :",
+      error
+    );
+    return res
+      .status(500)
+      .json({
+        error:
+          "Erreur serveur lors de la récupération des infos des vols de l'utilisateur",
+      });
   }
 });
-
-
-
 
 module.exports = router;
